e8a02d7d9dd0228ca8e87c3f215b06d06cbc7aba
german.lopezcampos@gmail.com <german.lopezcampos@gmail.com>
2019-08-17 18:53:47 +0100

    convert error to string

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ class RadioBot {
         }
         catch(e) {
            console.log(e)
-           cmd = `echo "${e}" > ${date}_error.txt`
+           cmd = `echo "${e.toString()}" > ${date}_error.txt`
            child_process.execSync(cmd)
 	         process.exit()
         }
